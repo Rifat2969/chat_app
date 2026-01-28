f6efefbc838a5e37fa9adbb88cee1fece2d97a19
imtiazahammed <ahammedrifat1920@gmail.com>
2026-01-28 11:26:14 +0600

    test

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -60,7 +60,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 obscureText: true,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter password';
+                    return 'Please enter your password';
                   }
                   if (value.length < 6) {
                     return 'Password must be at least 6 characters';
